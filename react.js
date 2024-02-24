7791160bd658d7b755a00d6fbbabcf5532855ad4
Vadim Niziev <st1mpro23@yandex.ru>
2024-02-25 00:50:39 +0300

    7.7 JSX

--- react.js
+++
@@ -1,7 +0,0 @@
-const button = React.createElement('button',{
-    type:"button",
-    disabled:true,
-    tabIndex:0,
-},'increment')
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(button);
