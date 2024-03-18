729cfa2a32f3841135c4ec4d15ff1938e75f9df0
Vadim Niziev <st1mpro23@yandex.ru>
2024-03-18 22:59:26 +0300

    4.1 - 4.3 Firebase init

--- src/script.tsx
+++ src/script.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './common.css';
 import { App } from './Components/App/App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { initializeAPI } from './api';
+
+initializeAPI();
 
 ReactDOM.render(
   <Router>
